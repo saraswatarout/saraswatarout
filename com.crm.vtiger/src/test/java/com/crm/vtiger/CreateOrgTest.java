5dfce8344fd6efedcdee4ac2209f0d93244dbc4b
ssd <ssd@DESKTOP-4V3O4JG>
2022-03-24 13:45:22 +0530

    new modification

--- com.crm.vtiger/src/test/java/com/crm/vtiger/CreateOrgTest.java
+++ com.crm.vtiger/src/test/java/com/crm/vtiger/CreateOrgTest.java
@@ -6,6 +6,8 @@ public class CreateOrgTest {
 	@Test
 	public void orgname() {
 		System.out.println("user 1 created");
+		System.out.println("user 2 modified");
+		System.out.println("user changes");
 	}
 
 }
