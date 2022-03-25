2c14c02a7eae11df08d60ae33adf31fe137352ba
ssd <ssd@DESKTOP-4V3O4JG>
2022-03-25 10:02:00 +0530

    commit

--- com.crm.vtiger/src/test/java/com/crm/invoice/CreateInvoice.java
+++ com.crm.vtiger/src/test/java/com/crm/invoice/CreateInvoice.java
@@ -3,8 +3,10 @@ package com.crm.invoice;
 public class CreateInvoice {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stubfchxf
 
+		
+		
 	}
 
 }
